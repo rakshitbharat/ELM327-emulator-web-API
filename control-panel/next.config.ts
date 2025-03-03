0ad53d562b1cf969e14fd9d94228003dba7b82ca
GitHub <rakshitbharat@gmail.com>
2025-03-03 17:54:08 +0530

    Remove unused dependencies and update Next.js configuration for improved setup

--- control-panel/next.config.ts
+++ control-panel/next.config.ts
@@ -1,18 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      "stream": require.resolve("stream-browserify"),
-    };
-    return config;
-  },
-}
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
 export default nextConfig;
